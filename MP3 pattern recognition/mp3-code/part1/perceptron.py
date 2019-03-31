fb50fc1236c4df064ea064b0b71d66d872ee1bf1
Abhi Kamboj <akamboj2@dcl-l416-04.ews.illinois.edu>
2019-03-31 18:28:38 -0500

    few syntax changes np.matmul vs @

--- MP3 pattern recognition/mp3-code/part1/perceptron.py
+++ MP3 pattern recognition/mp3-code/part1/perceptron.py
@@ -2,26 +2,26 @@ import numpy as np
 
 class MultiClassPerceptron(object):
 	def __init__(self,num_class,feature_dim):
-		"""Initialize a multi class perceptron model. 
+		"""Initialize a multi class perceptron model.
 
 		This function will initialize a feature_dim weight vector,
-		for each class. 
+		for each class.
 
 		The LAST index of feature_dim is assumed to be the bias term,
-			self.w[:,0] = [w1,w2,w3...,BIAS] 
+			self.w[:,0] = [w1,w2,w3...,BIAS]
 			where wi corresponds to each feature dimension,
-			0 corresponds to class 0.  
+			0 corresponds to class 0.
 
 		Args:
 		    num_class(int): number of classes to classify
-		    feature_dim(int): feature dimension for each example 
+		    feature_dim(int): feature dimension for each example
 		"""
 		self.num_class=num_class
 		self.w = np.zeros((feature_dim+1,num_class)) #doesn't adding bias to end of feature dim array mess it up? bias is supposed not be multiplied by the class value
 													# like it's y=Wx+b not y=x(W+b)
 													#^^^no bc when ur doing multiplication we're going to be appending 1's to end of x so it will end up being xW+1*b
 	def train(self,train_set,train_label,df,b):
-		""" Train perceptron model (self.w) with training dataset. 
+		""" Train perceptron model (self.w) with training dataset.
 
 		Args:
 		    train_set(numpy.ndarray): training examples with a dimension of (# of examples, feature_dim)
@@ -36,8 +36,8 @@ class MultiClassPerceptron(object):
 		for i,ex in enumerate(train_set):
 			correct=train_label[i]
 			while True:
-				ans=np.append(ex,1) @ self.w	#matrix multiplication
-				prediction=np.argmax(ans)	
+				ans=np.matmul(np.append(ex,1),self.w)	#matrix multiplication
+				prediction=np.argmax(ans)
 				if prediction==train_label[i]: #if prediciton is correct break
 					break
 				#print(lr_arr[correct])
@@ -50,34 +50,34 @@ class MultiClassPerceptron(object):
 		pass
 
 	def test(self,test_set,test_label):
-		""" Test the trained perceptron model (self.w) using testing dataset. 
-			The accuracy is computed as the average of correctness 
-			by comparing between predicted label and true label. 
-			
+		""" Test the trained perceptron model (self.w) using testing dataset.
+			The accuracy is computed as the average of correctness
+			by comparing between predicted label and true label.
+
 		Args:
 		    test_set(numpy.ndarray): testing examples with a dimension of (# of examples, feature_dim)
 		    test_label(numpy.ndarray): testing labels with a dimension of (# of examples, )
 
 		Returns:
-			accuracy(float): average accuracy value 
+			accuracy(float): average accuracy value
 			pred_label(numpy.ndarray): predicted labels with a dimension of (# of examples, )
-		"""    
+		"""
 
 		# YOUR CODE HERE
-		accuracy = 0 
+		accuracy = 0
 		pred_label = np.zeros((len(test_set)))
 		classif_rate=np.zeros(self.num_class) #counts correctly labeled examples (like numerator of classification rate)
 		true_labels_count=np.zeros(self.num_class)	# counts all labels in test set (like denomenator of classification rate)
 
 		for i,ex in enumerate(test_set):
-			ans=np.append(ex,1) @ self.w
+			ans=np.matmul(np.append(ex,1), self.w)
 			prediction=np.argmax(ans)
 			correct=test_label[i]
 			pred_label[i]=prediction
 			if correct==prediction:
 				classif_rate+=1
 			true_labels_count+=1
-		
+
 		classif_rate/=true_labels_count #element wise division
 		accuracy=np.average(classif_rate)
 	#	print("classification rates:",classif_rate)
@@ -86,14 +86,13 @@ class MultiClassPerceptron(object):
 		return accuracy, pred_label
 
 	def save_model(self, weight_file):
-		""" Save the trained model parameters 
-		""" 
+		""" Save the trained model parameters
+		"""
 
 		np.save(weight_file,self.w)
 
 	def load_model(self, weight_file):
-		""" Load the trained model parameters 
-		""" 
+		""" Load the trained model parameters
+		"""
 
 		self.w = np.load(weight_file)
-
